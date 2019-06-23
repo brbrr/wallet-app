837ce5e14e119d6f3627c40eab017f4f76e81ec5
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-06-23 14:42:26 +0300

    Extract AccountInfo component

---
+++ src/components/AccountInfo.js
@@ -0,0 +1,152 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { StyleSheet, ScrollView, Platform } from 'react-native';
+import { ListItem, Input } from 'react-native-elements';
+import PropTypes from 'prop-types';
+
+export default class AccountInfo extends React.Component {
+	render() {
+		const { navigate, onStateChange, name, balance, colorCode, iconName, currencyCode } = this.props;
+		return (
+			<ScrollView style={ styles.container } keyboardShouldPersistTaps="always" >
+				<ListItem
+					title="Name"
+					rightTitle={ <Input
+						value={ name }
+						placeholder="Account name"
+						onChangeText={ ( n ) => onStateChange( { name: n } ) }
+						autoFocus
+
+						inputContainerStyle={ { borderBottomWidth: 0 } }
+						inputStyle={ styles.amountInput }
+					/> }
+					bottomDivider={ true }
+					topDivider={ true }
+
+					containerStyle={ styles.itemContainer }
+					contentContainerStyle={ { flex: 2 } }
+					rightContentContainerStyle={ { flex: 4 } }
+				/>
+
+				<ListItem
+					title="Balance"
+					rightTitle={ <Input
+						value={ balance }
+						placeholder="0.0"
+						keyboardType="numeric"
+						onChangeText={ ( b ) => onStateChange( { balance: b } ) }
+
+						inputContainerStyle={ { borderBottomWidth: 0 } }
+						inputStyle={ styles.amountInput }
+					/> }
+					bottomDivider={ true }
+					topDivider={ true }
+
+					containerStyle={ styles.itemContainer }
+					contentContainerStyle={ { flex: 2 } }
+					rightContentContainerStyle={ { flex: 4 } }
+				/>
+				<ListItem
+					title="Currency"
+					rightTitle={ currencyCode }
+					bottomDivider={ true }
+					topDivider={ true }
+					onPress={ () => navigate( 'SettingsCurrencies', { onStateChange } ) }
+
+					containerStyle={ StyleSheet.flatten( [ styles.itemContainer ] ) }
+					contentContainerStyle={ { flex: 2 } }
+					rightContentContainerStyle={ { flex: 4 } }
+				/>
+
+				<ListItem
+					title={ 'Color' }
+					bottomDivider={ true }
+					topDivider={ true }
+					leftIcon={ {
+						name: 'circle',
+						type: 'font-awesome',
+						color: colorCode,
+						size: 42,
+						containerStyle: { margin: -2 },
+					} }
+					chevron
+					rightTitle={ 'Select' }
+					onPress={ () => navigate( 'ColorSelector', { onStateChange } ) }
+
+					containerStyle={ styles.itemContainer }
+					contentContainerStyle={ { flex: 2 } }
+					rightContentContainerStyle={ { flex: 1 } }
+				/>
+
+				<ListItem
+					title={ 'Icon ' + iconName }
+					bottomDivider={ true }
+					topDivider={ true }
+					leftIcon={ {
+						name: iconName,
+						type: 'font-awesome',
+						color: 'black',
+						size: 42,
+						containerStyle: { margin: -2 },
+					} }
+					chevron
+					rightTitle={ 'Select' }
+					onPress={ () => navigate( 'IconSelector', { onStateChange } ) }
+
+					containerStyle={ styles.itemContainer }
+					contentContainerStyle={ { flex: 2 } }
+					rightContentContainerStyle={ { flex: 1 } }
+				/>
+			</ScrollView>
+		);
+	}
+}
+
+const styles = StyleSheet.create( {
+	container: {
+		flex: 1,
+		backgroundColor: '#eee',
+
+		...Platform.select( {
+			ios: {
+				paddingTop: 20,
+			},
+		} ),
+	},
+	itemContainer: {
+		height: 55,
+		flexDirection: 'row',
+		paddingTop: 3,
+		paddingBottom: 3,
+	},
+	currencyButton: {
+		color: 'grey',
+		padding: 3,
+		borderWidth: 1,
+		borderColor: 'grey',
+		borderRadius: 3,
+		backgroundColor: '#f9f9f9',
+		marginLeft: 7,
+		marginRight: 9,
+	},
+	amountTitle: { fontSize: 12, marginTop: 2 },
+	amountInput: { color: 'black', textAlign: 'right' },
+	colorBox: ( size, color ) => ( {
+		width: size,
+		height: size,
+		backgroundColor: color,
+		margin: 3,
+	} ),
+} );
+
+AccountInfo.propTypes = {
+	onStateChange: PropTypes.func.isRequired,
+	navigate: PropTypes.func.isRequired,
+	name: PropTypes.string.isRequired,
+	balance: PropTypes.string.isRequired,
+	colorCode: PropTypes.string.isRequired,
+	iconName: PropTypes.string.isRequired,
+	currencyCode: PropTypes.string.isRequired,
+};
--- src/screens/NewAccountScreen.js
+++ src/screens/NewAccountScreen.js
@@ -2,14 +2,15 @@
  * External dependencies
  */
 import React from 'react';
-import { Button, StyleSheet, ScrollView } from 'react-native';
+import { Button } from 'react-native';
 import { connect } from 'react-redux';
-import { ListItem, Input } from 'react-native-elements';
 
 /**
  * Internal dependencies
  */
 import { addNewAccount } from '../actions';
+import AccountInfo from '../components/AccountInfo';
+import { getCurrencyById } from '../selectors';
 
 class NewAccountScreen extends React.Component {
 	static navigationOptions = ( { navigation } ) => ( {
@@ -32,107 +33,52 @@ class NewAccountScreen extends React.Component {
 		super( props );
 		this.state = {
 			name: null,
-			balance: null,
+			balance: 0.0,
+			colorCode: 'blue',
+			iconName: 'car',
+			currencyId: 1,
 		};
+
 		this.props.navigation.setParams(
-			{ createNewAccountAndGoBack: this.createNewAccountAndGoBack.bind( this ) }
+			{ createNewAccountAndGoBack: this.createNewAccountAndGoBack }
 		);
 	}
 
-	createNewAccountAndGoBack() {
-		const { name, balance } = this.state;
-		const { navigation, _addNewAccount } = this.props;
-		const color = navigation.getParam( 'colorCode', 'blue' );
-		const icon = navigation.getParam( 'iconName', 'car' );
-		const currencyId = navigation.getParam( 'currencyId', 1 );
+	onStateChange = ( state ) => this.setState( state )
 
-		_addNewAccount( { name, color, icon, currencyId, balance } );
+	createNewAccountAndGoBack = () => {
+		const { name, balance, colorCode, iconName, currencyId } = this.state;
+		const { navigation, _addNewAccount } = this.props;
+
+		_addNewAccount( { name, colorCode, iconName, currencyId, balance } );
 		navigation.goBack();
 	}
 
 	render() {
-		const { name, balance } = this.state;
+		const { name, balance, colorCode, iconName, currencyId } = this.state;
 		const { navigation } = this.props;
-		const colorCode = navigation.getParam( 'colorCode', 'blue' );
-		const iconName = navigation.getParam( 'iconName', 'car' );
+
+		const currency = getCurrencyById( this.props, currencyId );
+
 		return (
-			<ScrollView style={ styles.container } keyboardShouldPersistTaps="always" >
-				<ListItem
-					containerStyle={ styles.iconContainer }
-					title="Name"
-					rightTitle={ <Input
-						inputContainerStyle={ { borderBottomWidth: 0 } }
-						inputStyle={ styles.amountInput }
-						value={ name }
-						placeholder="Account name"
-						onChangeText={ ( n ) => this.setState( { name: n } ) }
-						autoFocus
-					/> }
-					contentContainerStyle={ { flex: 2 } }
-					rightContentContainerStyle={ { flex: 4 } }
-					bottomDivider={ true }
-					topDivider={ true }
-				/>
-
-				<ListItem
-					containerStyle={ styles.iconContainer }
-					title="Balance"
-					rightTitle={ <Input
-						inputContainerStyle={ { borderBottomWidth: 0 } }
-						inputStyle={ styles.amountInput }
-						value={ balance }
-						placeholder="0.0"
-						keyboardType="numeric"
-						onChangeText={ ( b ) => this.setState( { balance: b } ) }
-					/> }
-					contentContainerStyle={ { flex: 2 } }
-					rightContentContainerStyle={ { flex: 4 } }
-					bottomDivider={ true }
-					topDivider={ true }
-				/>
-
-				<ListItem
-					containerStyle={ { paddingTop: 3, paddingBottom: 3, height: 55 } }
-					contentContainerStyle={ { flex: 2 } }
-					rightContentContainerStyle={ { flex: 1 } }
-					title={ 'Color' }
-					bottomDivider={ true }
-					topDivider={ true }
-					leftIcon={ {
-						name: 'circle',
-						type: 'font-awesome',
-						color: colorCode,
-						size: 42,
-						containerStyle: { margin: -2 },
-					} }
-					chevron
-					rightTitle={ 'Select' }
-					onPress={ () => navigation.navigate( 'ColorSelector', { parent: 'NewAccount' } ) }
-				/>
-
-				<ListItem
-					containerStyle={ { paddingTop: 3, paddingBottom: 3, height: 55 } }
-					contentContainerStyle={ { flex: 2 } }
-					rightContentContainerStyle={ { flex: 1 } }
-
-					title={ 'Icon ' + iconName }
-					bottomDivider={ true }
-					topDivider={ true }
-					leftIcon={ {
-						name: iconName,
-						type: 'font-awesome',
-						color: 'black',
-						size: 42,
-						containerStyle: { margin: -2 },
-					} }
-					chevron
-					rightTitle={ 'Select' }
-					onPress={ () => navigation.navigate( 'IconSelector', { parent: 'NewAccount' } ) }
-				/>
-			</ScrollView>
+			<AccountInfo
+				name={ name }
+				balance={ balance }
+				colorCode={ colorCode }
+				iconName={ iconName }
+				currencyCode={ currency.code }
+				navigate={ navigation.navigate }
+				onStateChange={ this.onStateChange } />
 		);
 	}
 }
+
+const mapStateToProps = ( state ) => {
+	const { currencies } = state;
+	return {
+		currencies,
+	};
+};
 
 const mapDispatchToProps = ( dispatch ) => {
 	return {
@@ -141,34 +87,6 @@ const mapDispatchToProps = ( dispatch ) => {
 };
 
 export default connect(
-	() => ( {} ),
+	mapStateToProps,
 	mapDispatchToProps
 )( NewAccountScreen );
-
-const styles = StyleSheet.create( {
-	container: { backgroundColor: '#f9f9f9', flex: 1 },
-	iconContainer: {
-		marginTop: 20,
-		marginBottom: 20,
-		height: 55,
-		flexDirection: 'row',
-	},
-	currencyButton: {
-		color: 'grey',
-		padding: 3,
-		borderWidth: 1,
-		borderColor: 'grey',
-		borderRadius: 3,
-		backgroundColor: '#f9f9f9',
-		marginLeft: 7,
-		marginRight: 9,
-	},
-	amountTitle: { fontSize: 12, marginTop: 2 },
-	amountInput: { color: 'black', textAlign: 'right' },
-	colorBox: ( size, color ) => ( {
-		width: size,
-		height: size,
-		backgroundColor: color,
-		margin: 3,
-	} ),
-} );
