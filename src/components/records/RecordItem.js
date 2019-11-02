99b3e7837a05be367cda46fa4fc2450a592a21bb
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-11-02 14:28:42 +0200

    Add TransferItem UI element

--- src/components/records/RecordItem.js
+++ src/components/records/RecordItem.js
@@ -44,6 +44,40 @@ const RecordsItem = ( { category, accountColor, amount, record, navigateEditReco
 	);
 };
 
+export const TransferItem = ( { accountColor, amount, record, navigateEditRecordScreen } ) => {
+	return (
+		<ListItem
+			containerStyle={ { paddingTop: 3, paddingBottom: 3 } }
+			title="Transfer"
+
+			rightContentContainerStyle={ { flex: 1 } }
+
+			rightTitle={ amount }
+			rightTitleStyle={ { fontWeight: 'bold', color: 'black', fontSize: 14 } }
+
+			subtitle={ record.description }
+			subtitleStyle={ { fontSize: 12, color: 'gray' } }
+
+			rightSubtitle={ getTime( record.createdAt ) }
+			rightSubtitleStyle={ { fontSize: 12 } }
+			leftIcon={
+				<View >
+					<Icon
+						name="exchange"
+						type="font-awesome"
+						reverse
+						reverseColor="white"
+						color={ 'black' }
+						size={ 20 }
+					/>
+					<View style={ styles.accountIndicator( accountColor ) } />
+				</View>
+			}
+			onPress={ () => navigateEditRecordScreen( record.id ) }
+		/>
+	);
+};
+
 const styles = StyleSheet.create( {
 	accountIndicator: ( color ) => ( {
 		width: 10,
