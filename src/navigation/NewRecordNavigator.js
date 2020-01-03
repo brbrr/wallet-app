1e25be4e42179ebbc02161a4b18251f7e2c4275c
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-01-03 15:39:06 +0200

    navigators update

--- src/navigation/NewRecordNavigator.js
+++ src/navigation/NewRecordNavigator.js
@@ -7,16 +7,32 @@ import { createStackNavigator } from 'react-navigation';
  * Internal dependencies
  */
 import NewRecordModal from '../screens/NewRecordModal';
-import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoriesScreen, { EditCategoriesScreen } from '../screens/CategoriesScreen';
 import CurrenciesScreen from '../screens/CurrenciesScreen';
 import AccountsScreen from '../screens/AccountsScreen';
 import { NewAccountStack } from './AccountsNavigator';
 import { NewCurrencyStack } from './CurrenciesNavigator';
 import { NewCategoryStack } from './CategoriesStack';
+import SubCategoriesScreen, { EditSubCategoriesScreen } from '../screens/SubCategoriesScreen';
 
-export const CategoriesStack = createStackNavigator( {
-	CategoriesList: { screen: CategoriesScreen },
+export const EditCategoriesStack = createStackNavigator( {
+	EditCategories: { screen: EditCategoriesScreen },
+	EditSubCategories: { screen: EditSubCategoriesScreen },
 	NewCategory: NewCategoryStack,
+} );
+
+export const ViewCategoriesStack = createStackNavigator( {
+	Categories: { screen: CategoriesScreen },
+	SubCategories: { screen: SubCategoriesScreen },
+} );
+
+export const CategoriesListStack = createStackNavigator( {
+	ViewCategories: ViewCategoriesStack,
+	EditCategories: { screen: EditCategoriesStack, navigationOptions: { header: null } },
+},
+{
+	mode: 'modal',
+	headerMode: 'none',
 } );
 
 export const AccountsStack = createStackNavigator( {
@@ -33,5 +49,5 @@ export default createStackNavigator( {
 	New: { screen: NewRecordModal },
 	Currencies: { screen: CurrenciesStack, navigationOptions: { header: null } },
 	Accounts: { screen: AccountsStack, navigationOptions: { header: null } },
-	Categories: { screen: CategoriesStack, navigationOptions: { header: null } },
+	Categories: { screen: CategoriesListStack, navigationOptions: { header: null } },
 } );
